483a3439dd31313272f6fb9d59efc9bd30086008
drjeko <jeko@cyberservices.com>
2016-04-27 10:14:08 -0400

    Create sniffer.py
    Quick little packet sniffer.  This needs to be run as root.

---
+++ sniffer.py
@@ -0,0 +1,28 @@
+!/usr/bin/env python
+import socket
+import os
+
+#------------------------------------------
+#Packet Information - Quick Sniffing stuff
+#------------------------------------------
+
+#creating a INET, raw socket
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+#clearing the screen cause messy screen suck :)
+cls = os.popen('clear')
+clear = cls.read()
+print(clear)
+
+print("__________________________________________________________________________________")
+print('Sniffing Packets....') 
+while True:
+  data = s.recvfrom(65565)
+  print(data) 
+  print("__________________________________________________________________________________")
+  
+  
+  
+  
+  
+  
